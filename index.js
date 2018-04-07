a74889efa36b45722accf9fd31500d0b5ed4c4a0
sgg2123 <sgg2123@columbia.edu>
2018-04-07 01:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
   return "YES INDEED!"
   }
-  if ("I love you, Grandma" === string) {
+  if ("I love you, Grandma." === string) {
   return "I love you, too." 
   }
 }
